49474e9fc8a690a6cca672435749f18f1dd22211
sakuti <saku.tihverainen@outlook.com>
2024-02-05 18:39:48 +0200

    Revert "App router & Redux"
    This reverts commit 5e6011f981a1f0dc8e4e2626a2d150040c3173e0.

--- app/testing/layout.tsx
+++
@@ -1,7 +0,0 @@
-interface TestLayoutProps {
-	children: React.ReactNode
-}
-
-export default function TestLayout({ children }: TestLayoutProps) {
-	return <div className="min-h-screen">{children}</div>
-}
--- redux/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import counterReducer from '@/redux/features/counterSlice';
-
-const rootReducer = combineReducers({
-	counter: counterReducer,
-});
-
-export default rootReducer;
